c2944e7e7fc95bed4b21c3d8d17121a17f4225fe
Dan Cohen <dan.cohen@euromoneyplc.com>
2015-11-12 17:16:10 +0000

    Graph being built properly

--- src/main/java/ConceptGraph/App.java
+++ src/main/java/ConceptGraph/App.java
@@ -1,6 +1,7 @@
 package ConceptGraph;
 
 import com.sun.jndi.toolkit.url.Uri;
+import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,6 +11,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -28,27 +30,37 @@ public class App {
 
         try {
             while (!Links.isEmpty()) {
-                if (Links.size() > 2000) {
+                if(QueriedUris.size() > 20){
                     return;
                 }
                 Uri curr = Links.remove();
+                System.out.println("quering " + curr.toString() + System.getProperty("line.separator"));
                 try {
                     Document doc = Jsoup.connect(curr.toString()).get();
 
                     String title = doc.getElementById("firstHeading").text();
                     Node node;
-                    if(!Nodes.containsKey(title)){
+                    if (!Nodes.containsKey(title)) {
                         node = new Node(title);
-                    }
-                    else{
+                    } else {
                         node = Nodes.get(title);
                     }
                     node.hasWikiPage = true;
                     Nodes.put(title, node);
 
                     QueriedUris.add(curr);
+                    System.out.println(QueriedUris.size() + " queried so far...");
                     String mainBody = doc.getElementById("mw-content-text").text();
+                    // currently case sensitive
                     String[] words = mainBody.split("\\W");
+
+                    Object[] filtered = Stream.of(words)
+                            .map((word) -> word.toLowerCase().trim())
+                            .filter((word) -> !word.isEmpty())
+                            .toArray();
+
+                    words = Arrays.copyOf(filtered, filtered.length, String[].class);
+
                     addWordsToHashSet(words);
                     getUrisFromDocument(doc);
                     getNodesFromWords(words, node);
@@ -56,27 +68,33 @@ public class App {
                     e.printStackTrace();
                 }
             }
-        } finally{
+        } finally {
             WriteOutput();
         }
     }
 
     private static void getNodesFromWords(String[] words, Node root) {
-        for(String word: words) {
-            Node node;
-            if (!Nodes.containsKey(word)) {
+        for (String word : words) {
+            Node node = Nodes.get(word);
+
+            if(node == null){
                 node = new Node(word);
-            }
-            else{
-                node = Nodes.get(word);
+                Nodes.put(word, node);
             }
 
             int strength = 1;
-            if(root.connectedConcepts.containsKey(node)){
+            if (root.connectedConcepts.containsKey(node)) {
                 strength = root.connectedConcepts.get(node) + 1;
             }
 
             root.connectedConcepts.put(node, strength);
+
+            strength = 1;
+            if (node.connectedConcepts.containsKey(root)) {
+                strength = node.connectedConcepts.get(root) + 1;
+            }
+
+            node.connectedConcepts.putIfAbsent(root, strength);
         }
     }
 
@@ -86,14 +104,20 @@ public class App {
         for (Element link : links) {
             String href = link.attr("href");
             if (href.startsWith("/")) {
-                href = "https://www.wikipedia.org" + href;
-                try {
-                    Uri uri = new Uri(href);
-                    if (SeenLinks.add(uri)) {
-                        Links.add(uri);
+                if (href.startsWith("//")) {
+                    href = "http:" + href;
+                } else {
+                    href = "https://www.wikipedia.org" + href;
+                }
+                if(href.contains("wikipedia.org")) {
+                    try {
+                        Uri uri = new Uri(href);
+                        if (SeenLinks.add(uri)) {
+                            Links.add(uri);
+                        }
+                    } catch (MalformedURLException e) {
+                        System.out.println(e.getLocalizedMessage());
                     }
-                } catch (MalformedURLException e) {
-                    System.out.println(e.getLocalizedMessage());
                 }
             }
         }
@@ -105,7 +129,7 @@ public class App {
                 if (!Frequency.containsKey(word)) {
                     Frequency.put(word, 0);
                 }
-                Frequency.merge(word, 1, (oldvalue, one) -> oldvalue + one);
+                Frequency.merge(word, 1, (oldValue, one) -> oldValue + one);
             }
         }
     }
@@ -123,7 +147,7 @@ public class App {
 
         FileWriter queried = new FileWriter("queried.txt");
 
-        for(Uri uri : QueriedUris){
+        for (Uri uri : QueriedUris) {
             queried.write(uri.toString() + System.getProperty("line.separator"));
         }
 
@@ -132,7 +156,7 @@ public class App {
         QueriedUris.clear();
 
         FileWriter nodes = new FileWriter("graph.txt");
-        for(Node node : Nodes.values()){
+        for (Node node : Nodes.values()) {
             nodes.write(node.toString() + System.getProperty("line.separator") + System.getProperty("line.separator"));
         }
 
